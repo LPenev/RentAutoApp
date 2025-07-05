e712e906a96baaec7133bc38ab763f77bef95be7
LPenev <150725482+LPenev@users.noreply.github.com>
2025-07-05 22:09:10 +0200

    RentAutoApp Structure and References created

---
+++ src/RentAutoApp.Data/RentAutoAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentAutoApp.Web.Data;
+
+public class RentAutoAppDbContext : IdentityDbContext
+{
+    public RentAutoAppDbContext(DbContextOptions<RentAutoAppDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ src/RentAutoApp.Web/RentAutoApp.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RentAutoApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
