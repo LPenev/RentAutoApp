052875637243e7f86d57e7335bd4196b87ae12ea
LPenev <150725482+LPenev@users.noreply.github.com>
2025-08-10 11:48:05 +0200

    RentAutoApp Program.cs updated

--- src/RentAutoApp.Web/Program.cs
+++ src/RentAutoApp.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using RentAutoApp.Data.Models;
+using RentAutoApp.Data.Seeding;
 using RentAutoApp.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,12 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<RentAutoAppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<RentAutoAppDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>(opt =>
+    {
+        opt.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddEntityFrameworkStores<RentAutoAppDbContext>()
+    .AddDefaultTokenProviders();
 
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<DbSeeder>();
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
@@ -27,6 +39,13 @@ builder.Services.AddAntiforgery(options =>
 
 
 var app = builder.Build();
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+    await seeder.SeedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -51,6 +70,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
