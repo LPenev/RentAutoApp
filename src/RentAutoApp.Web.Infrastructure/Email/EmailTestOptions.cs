b9f373201ff713a7f7984aee260f2817fba2789c
LPenev <150725482+LPenev@users.noreply.github.com>
2025-09-11 14:43:13 +0200

    RentAutoApp EmailTestOptions

---
+++ src/RentAutoApp.Web.Infrastructure/Email/EmailTestOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentAutoApp.Web.Infrastructure.Email;
+
+public class EmailTestOptions
+{
+    public bool Enabled { get; set; } = true;
+    [EmailAddress] 
+    public string? DefaultRecipient { get; set; }
+    [Required] 
+    public string Subject { get; set; } = "Test email";
+    [Required] 
+    public string BodyHtml { get; set; } = "<b>It works!</b>";
+}
