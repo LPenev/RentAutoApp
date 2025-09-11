c35868c8e473e4c4c3f7cdca73c91d084e608e6a
LPenev <150725482+LPenev@users.noreply.github.com>
2025-09-11 14:46:22 +0200

    RentAutoApp IServiceCollection added

---
+++ src/RentAutoApp.Web.Infrastructure/Email/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RentAutoApp.Web.Infrastructure.Email;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEmailTestOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddOptions<EmailTestOptions>()
+            .Bind(configuration.GetSection("EmailTest"))
+            .ValidateDataAnnotations();
+        return services;
+    }
+}
