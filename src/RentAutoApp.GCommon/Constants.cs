5a04748b1f8d54dc2b805055791e9539af83190e
LPenev <150725482+LPenev@users.noreply.github.com>
2025-09-24 18:21:11 +0200

    RentAutoApp Localication of Vehicle completed

--- src/RentAutoApp.GCommon/Constants.cs
+++ src/RentAutoApp.GCommon/Constants.cs
@@ -2,19 +2,11 @@
 
 public class Constants
 {
-    public class Vehicle
-    { 
-        public const string VehicleIsAvailable = "Налична";
-        public const string VehicleNotAvailable = "Не е налична";
-        public const string LabelBack = "Обратно";
-        public const string NotAvailableForRent = "Не е налична за наем";
-    }
 
     public class Contact
     {
         public const string LabelContactEmailSentTo = "Contact email sent to {Recipient}";
         public const string LabelNewRequestFromSite = "Ново запитване от сайта";
-        public const string LabelContactOK = "Благодарим за съобщението! Ще се свържем с вас скоро.";
     }
 
     public const string CultureBulgarian = "bg";
